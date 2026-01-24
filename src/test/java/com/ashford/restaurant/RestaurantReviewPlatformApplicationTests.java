6a8c8761a760928e24791be0535bf082fba434a3
LAPTOP-QA62HH1U\saimo <saimohanmajoju@gmail.com>
2026-01-24 16:05:31 -0500

    initial commit

---
+++ src/test/java/com/ashford/restaurant/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashford.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
